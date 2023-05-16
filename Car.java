c723c6f00283f8094c6252f5819e3b9fe0a99344
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-16 22:47:43 +0300

    Update Car.java
    common getter and setter were added

--- Car.java
+++ Car.java
@@ -5,6 +5,34 @@ public class Car extends AVehicle {
     public ITransportable bl ;
     public ITransportable br ;
 
+    ///####   COMMON GETTER AND SETTER  ############
+
+    public ITransportable getThingFromPlace (String place){
+        if (place.equals("Front Left")){
+            return fl;
+        }else if (place.equals("Front right")) {
+            return fr;
+        }else if (place.equals("Back left")) {
+            return br;
+        }else if (place.equals("Back right")) {
+            return br;
+        } 
+        return null;
+    }
+
+    public void setThingToPlace(String place, ITransportable thing){
+        if (place.equals("Front Left")){
+            fl = (Driver)thing;
+        }else if (place.equals("Front right")) {
+             fr = thing;
+        }else if (place.equals("Back right")) {
+             br = thing;
+        }else if (place.equals("Back left")) {
+             bl = thing;
+        } 
+    }
+    //##############################################
+
     public String toString(){
         String out;
 
@@ -27,3 +55,4 @@ public class Car extends AVehicle {
 
     }
 }
+
