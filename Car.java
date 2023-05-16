ba0fc3268fa9a47d15aa4fe7be87a7b8eb3c7f65
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-16 22:46:18 +0300

    Update Car.java
    common getter and setter were added

--- Car.java
+++ Car.java
@@ -26,4 +26,61 @@ public class Car extends AVehicle {
         return out;
 
     }
+}public class Car extends AVehicle {
+    public Driver fl ;
+
+    public ITransportable  fr ;
+    public ITransportable bl ;
+    public ITransportable br ;
+
+    ///####   COMMON GETTER AND SETTER  ############
+
+    public ITransportable getThingFromPlace (String place){
+        if (place.equals("Front Left")){
+            return fl;
+        }else if (place.equals("Front right")) {
+            return fr;
+        }else if (place.equals("Back left")) {
+            return br;
+        }else if (place.equals("Back right")) {
+            return br;
+        } 
+        return null;
+    }
+
+    public void setThingToPlace(String place, ITransportable thing){
+        if (place.equals("Front Left")){
+            fl = (Driver)thing;
+        }else if (place.equals("Front right")) {
+             fr = thing;
+        }else if (place.equals("Back right")) {
+             br = thing;
+        }else if (place.equals("Back left")) {
+             bl = thing;
+        } 
+    }
+    //##############################################
+
+    public String toString(){
+        String out;
+
+        out = 
+
+            "+------Object<Car>-------+\n" +
+            "|                        |\n" +
+            "|  +----+        +----+  |\n" +
+            "|  |"+fl+"|        |"+fr+"|  |\n" +
+            "|  +----+        +----+  |\n" +
+            "|                        |\n" +
+            "|                        |\n" +
+            "|  +----+        +----+  |\n" +
+            "|  |"+bl+"|        |"+br+"|  |\n" +
+            "|  +----+        +----+  |\n" +
+            "|                        |\n" +
+            "+------------------------+\n";
+
+        return out;
+
+    }
 }
+
